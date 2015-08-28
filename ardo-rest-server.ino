5f688a5f706902dcaf231971cd4d1bf1a3ad7280
Mitchel Verschoof <mitchel@future500.nl>
2015-08-28 10:59:39 +0200

    Added temperature route

--- ardo-rest-server.ino
+++ ardo-rest-server.ino
@@ -7,7 +7,7 @@
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x0E, 0xFE, 0x40 };
 
 // IP address in case DHCP fails
-IPAddress ip(10,0,0,3);
+IPAddress ip(10, 0, 0, 3);
 
 // Ethernet server
 EthernetServer server(80);
@@ -15,19 +15,21 @@ EthernetServer server(80);
 // Create aREST instance
 aREST rest = aREST();
 
-/* MUSIC */
-int speakerPin = 9;
+/* SET PINS */
+int speakerPin     = 9;
+int temperaturePin = 0;
 
 void setup(void)
-{  
+{
   pinMode(speakerPin, OUTPUT);
 
   // Start Serial
   Serial.begin(9600);
-  
+
   // Function to be exposed
+  rest.function("temp", temperature);
   rest.function("play", playSong);
-  
+      
   // Give name and ID to device
   rest.set_id("001");
   rest.set_name("Ardo");
@@ -39,16 +41,16 @@ void setup(void)
     // try to congifure using IP address instead of DHCP:
     Ethernet.begin(mac, ip);
   }
-  
+
   server.begin();
   Serial.print("server is at ");
   Serial.println(Ethernet.localIP());
 }
 
-void loop() {  
+void loop() {
   // listen for incoming clients
   EthernetClient client = server.available();
-  
+
   rest.handle(client);
 }
 
@@ -58,7 +60,7 @@ void loop() {
 
 // Custom function accessible by the API
 int playSong(String command) {
-  
+
   int songSize   = command.length() + 1;
   int songLength = command.length();
   int tempo      = 150;
@@ -72,9 +74,9 @@ int playSong(String command) {
     } else {
       playNote(song[i], 1 * tempo);
     }
-    
+
     // pause between notes
-    delay(tempo / 2); 
+    delay(tempo / 2);
   }
 
   return 1;
@@ -92,11 +94,29 @@ void playTone(int tone, int duration) {
 void playNote(char note, int duration) {
   char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
   int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
-  
+
   // play the tone corresponding to the note name
   for (int i = 0; i < 8; i++) {
     if (names[i] == note) {
       playTone(tones[i], duration);
     }
   }
+}
+
+/* Route: temperature*/
+int temperature(String command) {
+  Serial.println("Starting temperature");
+  //getting the voltage reading from the tem
+  float temperature = getVoltage(temperaturePin);
+
+  // temperature sensor
+  temperature = (temperature - .5) * 100;
+  
+  return (int)temperature;
+}
+
+float getVoltage(int pin) {
+  //converting from a 0 to 1023 digital range
+  // to 0 to 5 volts (each 1 reading equals ~ 5 millivolts
+  return (analogRead(pin) * .004882814);
 }
